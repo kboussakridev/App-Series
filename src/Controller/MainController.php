043acf58dac8d7836e7a79b5283eead3de603503
karim boussakri <kboussakridev@gmail.com>
2024-02-25 15:21:01 +0100

    j'utilise twig pour mes templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,8 +13,7 @@ class MainController extends AbstractController
      */
     public function home()
     {
-        echo "coucou";
-        die();
+        return $this->render('main/home.html.twig');
     }
 
     /**
@@ -22,7 +21,6 @@ class MainController extends AbstractController
      */
     public function test()
     {
-        echo "testounet";
-        die();
+        return $this->render('main/test.html.twig');
     }
 }
