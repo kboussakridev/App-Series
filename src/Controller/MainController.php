ee27f4aa3939113c5179a8ebe328f0a5d6b31a55
karim boussakri <kboussakridev@gmail.com>
2024-02-26 21:33:57 +0100

    crée les pages de base pour les séries

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,14 +11,20 @@ class MainController extends AbstractController
     #[Route('/', name: 'main_home')]
     public function home(): Response
     {
-        return $this->render('main/home.html.twig', [
-            'controller_name' => 'MainControllerController',
-        ]);
+        return $this->render('main/home.html.twig');
     }
 
     #[Route('/test', name: 'main_test')]
     public function test(): Response
     {
-        return $this->render('main/test.html.twig');
+        $setie = [
+            "title" => "Game of Thrones",
+            "year" => 2000,
+        ];
+
+        return $this->render('main/test.html.twig',
+        [
+            "mySerie" => $setie
+        ]);
     }
 }
